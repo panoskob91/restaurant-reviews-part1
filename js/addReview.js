8be5bbeaee491d8f49e4bdf2f653e4807b44d7fb
Unknown <panoskob91@gmail.com>
2018-06-02 18:03:57 +0300

    Form fields validation added

--- js/addReview.js
+++ js/addReview.js
@@ -10,6 +10,7 @@ function addReview() {
         addReviewForm.setAttribute('id', 'add-review-form');
         addReviewForm.setAttribute('onsubmit', 'handleSubmit()');
         addReviewForm.setAttribute('action', '');
+        //addReviewForm.setAttribute('method', 'post');
         addReviewForm.setAttribute('display', 'block');
         addReviewForm.innerHTML = createFormHTML();
         container.appendChild(addReviewForm);
@@ -36,8 +37,8 @@ function addReview() {
 }
 
 function getSliderValue() {
-    // const slider = document.getElementsByName('rating-slider')[0];
-    const slider = document.getElementById('rating-slider');
+    const slider = document.getElementsByName('rating-slider')[0];
+    //const slider = document.getElementById('rating-slider');
     const ratingLabel = document.getElementById('rating-label');
     var sliderValue = slider.value;
     slider.oninput = function () {
@@ -49,14 +50,14 @@ function getSliderValue() {
 function createFormHTML() {
     //with name tag form fields are added to the URL
     const formHTML = 'Name <br>'
-        + '<input type =' + 'text' + ' id=' + 'reviewer-name' + '>' +
+        + '<input type =' + 'text' + ' name=' + 'reviewer-name' + ' id=' + 'reviewer-name' + '>' +
         '<br>'
         + 'Rating <br>'
-        + '<input type=' + 'range ' + 'min=' + '1 ' + 'max=' + '10' + ' value=' + '5' + ' id=' + 'rating-slider' +
+        + '<input type=' + 'range ' + 'min=' + '1 ' + 'max=' + '10' + ' value=' + '5' + ' name=' + 'rating-slider' +
         ' step=' + '1' + '>'
         + '<p id=' + 'rating-label' + '></p>'
         + 'Comment <br>'
-        + '<textarea rows=' + '10' + ' cols=' + '50' + ' id=' + 'comment-section' + '></textarea>'
+        + '<textarea rows=' + '10' + ' cols=' + '50' + ' name=' + 'comment-section' +  ' id =' + 'comment-section' + '></textarea>'
         + '<br>'
         + '<input type=' + ' submit' + '>';
 
@@ -65,15 +66,32 @@ function createFormHTML() {
 
 function handleSubmit() {
 
-    const name = document.getElementById('reviewer-name');
-    const restaurantRating = document.getElementById('rating-slider');
-    const commentSection = document.getElementById('comment-section');
+    // const name = document.getElementById('reviewer-name');
+    // const restaurantRating = document.getElementById('rating-slider');
+    // const commentSection = document.getElementById('comment-section');
 
+    validateForm();
 
     console.log('Correct');
 }
 
 /* ================================================================================================ */
+
+function validateForm() {
+    var name = document.forms['add-review-form']['reviewer-name'].value;
+    var comment = document.forms['add-review-form']['comment-section'].value;
+    if (name === "")
+    {
+        alert('Name must be filled out');
+        return false;
+    }
+    if (comment === '')
+    {
+        alert('Comment must be filled out');
+        return false;
+    }
+    return true;
+}
 
 function launchPopup(firstTime) {
     if (firstTime === true) {
